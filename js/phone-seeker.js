c0a8d0cf1bcc8eb5452655c3cf7bcf1fcdd135c9
ohnadir <qx.nadir@gmail.com>
2022-02-28 20:33:02 +0600

    get phone list

--- js/phone-seeker.js
+++ js/phone-seeker.js
@@ -6,7 +6,21 @@ const searchPhone = () =>{
     .then(data => loadPhone(data.data.slice(0,20)));
 }
 const loadPhone = data =>{
+    const phoneListContainer = document.getElementById('phone-list-container');
+    phoneListContainer.textContent = '';
     data.forEach(phone => {
-        console.log(phone.phone_name)
+        const div = document.createElement('div');
+        div.classList.add('col', 'mb-4');
+        div.innerHTML = `
+            <div class="card h-100 text-center">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text"> Card Details</p>
+                </div>
+            </div>
+        `
+        phoneListContainer.appendChild(div);
+        console.log(phone)
     });
 }
